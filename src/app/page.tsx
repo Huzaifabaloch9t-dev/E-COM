f366a4704f03af3740774c01443e08ea238b4825
huzaifa baloch <huzaifa.baloch.9t@gmail.com>
2026-02-06 18:46:45 -0800

    upate page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -76,7 +76,7 @@ export default function Home() {
       
       <nav className="bg-blue-600 text-white px-6 py-4 flex justify-between items-center">
         <h1 className="text-xl font-bold">Mini E-Commerce</h1>
-        <p>This is my E-Comerence Website</p>
+        
         <p>Online shopping website</p>
 
         <span className="bg-white text-blue-600 px-3 py-1 rounded-full text-sm font-semibold">
